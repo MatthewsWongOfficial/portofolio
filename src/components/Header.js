09f71d508ec2e391a2eb8dd62815a8e3cad7a2d9
MatthewsWongOfficial <matthewswong2610@gmail.com>
2024-01-11 20:25:52 +0700

    Improved Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,29 @@
-// src/components/Header.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
-import './Header.css'; // Import the CSS file for your styles
+import './Header.css';
 
 const Header = () => {
   const navLinkStyle = {
-    fontFamily: 'Roboto-Bold', // Set the font-family to Roboto-Bold
-    // Add any other styling you want for the navigation links
+    fontFamily: 'Roboto-Bold',
   };
 
   return (
     <Navbar bg="dark" variant="dark" expand="md" className="sticky-header">
       <Container>
-        <Navbar.Brand as={Link} to="landing-page" smooth duration={500} style={navLinkStyle}>
+        <Navbar.Brand as={Link} to="landing-page" smooth duration={500} style={{...navLinkStyle, marginBottom: '-2mm'}} offset={-50}>
           Matthews Wong
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="education" smooth duration={500} style={navLinkStyle}>
+            <Nav.Link as={Link} to="education" smooth duration={500} style={navLinkStyle} offset={-28}>
               Education
             </Nav.Link>
-            <Nav.Link as={Link} to="skills" smooth duration={500} style={navLinkStyle}>
+            <Nav.Link as={Link} to="skills" smooth duration={500} style={navLinkStyle} offset={-37}>
               Skills
             </Nav.Link>
-            <Nav.Link as={Link} to="certifications" smooth duration={500} style={navLinkStyle}>
+            <Nav.Link as={Link} to="certifications" smooth duration={500} style={navLinkStyle} offset={-22}>
               Certifications
             </Nav.Link>
             {/* Add more Nav.Link for other sections if needed */}
