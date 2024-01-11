82e66102e386b92ba0fc81920fe34a9491e9b6a4
MatthewsWongOfficial <matthewswong2610@gmail.com>
2024-01-11 20:36:22 +0700

    Improved Responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import './Header.css';
 
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavItemClick = () => {
+    setExpanded(false);
+  };
+
   const navLinkStyle = {
     fontFamily: 'Roboto-Bold',
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="md" className="sticky-header">
+    <Navbar bg="dark" variant="dark" expand="md" className="sticky-header" expanded={expanded}>
       <Container>
-        <Navbar.Brand as={Link} to="landing-page" smooth duration={500} style={{...navLinkStyle, marginBottom: '-2mm'}} offset={-50}>
+        <Navbar.Brand as={Link} to="landing-page" smooth duration={500} style={{ ...navLinkStyle, marginBottom: '-2mm' }} offset={-50}>
           Matthews Wong
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="education" smooth duration={500} style={navLinkStyle} offset={-28}>
+            <Nav.Link as={Link} to="education" smooth duration={500} style={navLinkStyle} offset={-28} onClick={handleNavItemClick}>
               Education
             </Nav.Link>
-            <Nav.Link as={Link} to="skills" smooth duration={500} style={navLinkStyle} offset={-37}>
+            <Nav.Link as={Link} to="skills" smooth duration={500} style={navLinkStyle} offset={-37} onClick={handleNavItemClick}>
               Skills
             </Nav.Link>
-            <Nav.Link as={Link} to="certifications" smooth duration={500} style={navLinkStyle} offset={-22}>
+            <Nav.Link as={Link} to="certifications" smooth duration={500} style={navLinkStyle} offset={-22} onClick={handleNavItemClick}>
               Certifications
             </Nav.Link>
             {/* Add more Nav.Link for other sections if needed */}
